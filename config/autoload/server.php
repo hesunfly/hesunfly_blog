7497e2d72a495747eafac0917d91725baabf9ef4
hesunfly <hesunfly@163.com>
2021-06-16 20:53:45 +0800

    envexapleme

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -29,7 +29,7 @@ return [
     ],
     'settings' => [
         Constant::OPTION_ENABLE_COROUTINE => true,
-        Constant::OPTION_WORKER_NUM => env('WORK_NUM', swoole_cpu_num()),
+        Constant::OPTION_WORKER_NUM => swoole_cpu_num(),
         Constant::OPTION_PID_FILE => BASE_PATH . '/runtime/hyperf.pid',
         Constant::OPTION_OPEN_TCP_NODELAY => true,
         Constant::OPTION_MAX_COROUTINE => 100000,
