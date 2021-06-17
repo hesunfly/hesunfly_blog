a7f0a3512940003fd686de876ca2b05a4e06523a
hesunfly <hesunfly@163.com>
2021-06-17 09:49:56 +0800

    fix

--- config/autoload/view.php
+++ config/autoload/view.php
@@ -13,7 +13,7 @@ use Hyperf\View\Mode;
 
 return [
     'engine' => \Hyperf\ViewEngine\HyperfViewEngine::class,
-    'mode' => Mode::TASK,
+    'mode' => Mode::SYNC,
     'config' => [
         'view_path' => BASE_PATH . '/storage/view/',
         'cache_path' => BASE_PATH . '/runtime/view/',
