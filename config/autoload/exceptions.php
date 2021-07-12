112da8eeda02c268ce22b70c9a276080c964308d
hesunfly <hesunfly@163.com>
2021-07-12 14:00:15 +0800

    处理异常捕获顺序

--- config/autoload/exceptions.php
+++ config/autoload/exceptions.php
@@ -12,15 +12,15 @@ declare(strict_types=1);
 return [
     'handler' => [
         'http' => [
-            App\Exception\Handler\AppExceptionHandler::class,
-            Hyperf\HttpServer\Exception\Handler\HttpExceptionHandler::class,
-            \Hyperf\ExceptionHandler\Handler\WhoopsExceptionHandler::class,
-            \Hyperf\ExceptionHandler\Listener\ErrorExceptionHandler::class,
             \Hyperf\Validation\ValidationExceptionHandler::class,
             \App\Exception\Handler\AuthExceptionHandler::class,
             \App\Exception\Handler\DbSaveExceptionHandler::class,
             \App\Exception\Handler\DbQueryExceptionHandler::class,
             \App\Exception\Handler\ModelNotFoundExceptionHandler::class,
+            \Hyperf\ExceptionHandler\Handler\WhoopsExceptionHandler::class,
+            \Hyperf\ExceptionHandler\Listener\ErrorExceptionHandler::class,
+            App\Exception\Handler\AppExceptionHandler::class,
+            Hyperf\HttpServer\Exception\Handler\HttpExceptionHandler::class,
         ],
     ],
 ];
