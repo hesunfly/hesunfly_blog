7a7d909da5184d8e338209f5cbd7833a1bcb3333
hesunfly <hesunfly@163.com>
2021-07-12 14:08:37 +0800

    处理异常捕获顺序

--- app/Exception/Handler/AppExceptionHandler.php
+++ app/Exception/Handler/AppExceptionHandler.php
@@ -15,6 +15,7 @@ use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\ExceptionHandler\ExceptionHandler;
 use Hyperf\HttpMessage\Stream\SwooleStream;
 use Psr\Http\Message\ResponseInterface;
+use Qbhy\HyperfAuth\Exception\UnauthorizedException;
 use Throwable;
 
 class AppExceptionHandler extends ExceptionHandler
@@ -31,6 +32,7 @@ class AppExceptionHandler extends ExceptionHandler
 
     public function handle(Throwable $throwable, ResponseInterface $response)
     {
+        var_dump($throwable instanceof UnauthorizedException);
         $this->logger->error(sprintf('%s[%s] in %s', $throwable->getMessage(), $throwable->getLine(), $throwable->getFile()));
         $this->logger->error($throwable->getTraceAsString());
         return $response->withHeader('Server', 'Hyperf')->withStatus(500)->withBody(new SwooleStream('Internal Server Error.'));
